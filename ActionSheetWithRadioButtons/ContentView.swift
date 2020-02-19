cecb360d858a395a586ec4359c20475e6d056e52
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-19 17:34:13 +0200

    added screenshots

--- ActionSheetWithRadioButtons/ContentView.swift
+++ ActionSheetWithRadioButtons/ContentView.swift
@@ -9,10 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selected = ""
+    @State var show = false
+    
     var body: some View {
         ZStack {
-            RadioButtons()
-        }
+            VStack {
+                Button(action: {
+                    self.show.toggle()
+                }) {
+                    Text("Open")
+                        .padding(.vertical)
+                        .padding(.horizontal, 25)
+                        .foregroundColor(.white)
+                }.background(LinearGradient(gradient: .init(colors: [Color("Color"), Color("Color1")]), startPoint: .leading, endPoint: .trailing))
+                .clipShape(Capsule())
+                
+                Text(self.selected)
+                    .padding(.top)
+            }
+            VStack {
+                Spacer()
+                RadioButtons(selected: self.$selected, show: self.$show)
+                    .offset(y: self.show ? (UIApplication.shared.windows.last?.safeAreaInsets.bottom)! + 15 : UIScreen.main.bounds.height)
+            }.background(Color(UIColor.label.withAlphaComponent(self.show ? 0.2 : 0)).edgesIgnoringSafeArea(.all))
+        }.background(Color("Color2").edgesIgnoringSafeArea(.all))
+        .animation(.default)
     }
 }
 
@@ -23,27 +45,65 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct RadioButtons: View {
+    @Binding var selected: String
+    @Binding var show: Bool
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            Text("FIlter By")
+            Text("Filter By")
+                .font(.title)
                 .padding(.top)
             
             ForEach(data, id: \.self) { i in
                 Button(action: {
-                    
+                    self.selected = i
                 }) {
                     HStack {
                         Text(i)
                         Spacer()
-                        Circle()
-                            .fill(Color.black.opacity(0.5))
-                            .frame(width: 20, height: 20)
-                    }
-                }
+                        ZStack {
+                            Circle()
+                                .fill(self.selected == i ? Color("Color") : Color.black.opacity(0.5))
+                                .frame(width: 18, height: 18)
+                            
+                            if self.selected == i {
+                                Circle()
+                                    .stroke(Color("Color1"), lineWidth: 4)
+                                    .frame(width: 25, height: 25)
+                            }
+                        }
+                    }.foregroundColor(.black)
+                }.padding()
             }
+            
+            HStack {
+                Spacer()
+            
+                Button(action: {
+                    self.show.toggle()
+                }) {
+                    Text("Continue")
+                        .padding(.vertical)
+                        .padding(.horizontal, 25)
+                        .foregroundColor(.white)
+                }
+                .background(
+                    self.selected != "" ?
+                    LinearGradient(gradient: .init(colors: [Color("Color"), Color("Color1")]), startPoint: .leading, endPoint: .trailing)
+                    :
+                        LinearGradient(gradient: .init(colors: [Color.black.opacity(0.2), Color.black.opacity(0.2)]), startPoint: .leading, endPoint: .trailing)
+                    )
+                    
+                .clipShape(Capsule())
+                .disabled(self.selected != "" ? false : true)
+                
+            }.padding(.top)
         }
         .padding(.vertical)
         .padding(.horizontal, 25)
+        .padding(.bottom, (UIApplication.shared.windows.last?.safeAreaInsets.bottom)! + 15)
+        .background(Color.white)
+        .cornerRadius(30)
     }
 }
 
